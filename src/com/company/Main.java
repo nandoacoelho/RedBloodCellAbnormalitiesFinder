cac60e25ef0ac6caf169ce454fdc7746d97586a3
nandoacoelho <Fernando@Macintosh.local>
2014-11-14 16:39:43 -0500

    Image Samples Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-
         System.load( "/usr/local/opencv/lib/libopencv_java2410.dylib" );
         Mat m=Highgui.imread("G4045749.jpg",Highgui.CV_LOAD_IMAGE_COLOR);
         new WindowDisplayer("G4045749.jpg",m);
